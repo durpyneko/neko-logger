0ebfadf5c825975a4ded4afd06314c8d9ad08f5e
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2024-01-29 03:21:18 +0200

    remove: double .name

--- how-to.js
+++ how-to.js
@@ -11,6 +11,6 @@ neko.name("NONE").info("This is a INFO message without UNDEF name")
 neko.warn("This is a WARN message.")
 neko.error("This is a ERROR message.\n")
 
-neko.name("FUNCTION1").info("This is a INFO message with FUNCTION 1 as name.").name("FUNC1")
+neko.name("FUNCTION1").info("This is a INFO message with FUNCTION 1 as name.")
 neko.name("FUNC1").warn("This is a WARN message with FUNCTION 1 as name.")
 neko.name("FUNC1").error("This is a ERROR message with FUNCTION 1 as name.")
