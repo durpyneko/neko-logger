1326dcdb4f35be9cf6956c6ff5388c964a7f0f31
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2024-01-29 15:39:15 +0200

    Update how-to.js

--- how-to.js
+++ how-to.js
@@ -8,9 +8,9 @@ const neko = new Logger({
 neko.log("Hello World!")
 neko.info("This is a INFO message.")
 neko.name("NONE").info("This is a INFO message without UNDEF name")
-neko.warn("This is a WARN message.")
+neko.color("yellow").warn("This is a WARN message in yellow.")
 neko.error("This is a ERROR message.\n")
 
 neko.name("FUNCTION1").info("This is a INFO message with FUNCTION 1 as name.")
 neko.name("FUNC1").warn("This is a WARN message with FUNCTION 1 as name.")
-neko.name("FUNC1").error("This is a ERROR message with FUNCTION 1 as name.")
+neko.name("FUNC1").color("red").error("This is a ERROR message with FUNCTION 1 as name and in red")
