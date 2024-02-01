d0e2cccc96292c0a8ced02b614490bb88389e3da
durpyneko <89787577+durpyneko@users.noreply.github.com>
2024-02-01 20:10:29 +0200

    add: chained error log

--- how-to.js
+++ how-to.js
@@ -17,4 +17,4 @@ neko.error("This is a ERROR message.\n")
 
 neko.name("FUNCTION1").info("This is a INFO message with FUNCTION 1 as name.")
 neko.name("FUNC1").warn("This is a WARN message with FUNCTION 1 as name.")
-neko.name("FUNC1").color("red").error("This is a ERROR message with FUNCTION 1 as name and in red")
+neko.name("FUNC1").color("red").error("This is a ERROR message with FUNCTION 1 as name and in red").error("new error")
